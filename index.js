38461527f4a8b6cf6efe4d4bf638b7abc9fded38
args10 <arijit.gupta@rwth-aachen.de>
2021-06-09 10:19:15 +0200

    chore: clean up scss etc.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 /**
- * Trying out "Better Comments"
- * Here I initially wanted to also use twitter API to get tweets. 
- * Since many people get there news diectly from twitter I thought it would be nice to show some tweets along with the news articles;
+ * Trying out VS code extension "Better Comments".
+ * This is a simple web app that uses newspi to 
+ * show latest news articles. I initially wanted
+ * to also use twitter API to show tweets along with news
+ * Since many people get there news diectly from 
+ * twitter I thought it would be nice to show some tweets along with the news articles;
  * ! But looks like the approval for access to twitter api might takes 2-3 days to come.
- * At Twitter each application is reviewed by a human being before granting acceess
- * ? Hence creating this application only with news articles newspi - access to there api is fast and easy! 
+ * ? At Twitter each application is reviewed by a human being before granting acceess
+ * Hence creating this application only with news articles from newspi. 
  */
 
 import './newzr-article.js'
 import {apiKey, topHeadLinesUrl} from './newsApi.js'
+// wait for the dom to load before alling fetchNews
 window.addEventListener('load',()=>{
     fetchNews();
 })
@@ -18,15 +22,16 @@ async function fetchNews(){
     const res = await fetch(topHeadLinesUrl);
     const json =await res.json();
 
+    //log all articles returned from newspi
     console.table(json.articles, ["title","description","content"]);
 
     const main = document.querySelector('main');
     json.articles.forEach(article => {
         const el = document.createElement('newzr-article');
+        //show articles having both images and a description
         if(article.urlToImage && article.description){
             el.article = article;
             main.appendChild(el);
         }
-        
     });
 }
